d6f060eeaca63fd70eb08d46a58bf87604b6af5b
LGiki <qq547221432@gmail.com>
2018-09-11 15:10:00 +0800

    Complete

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# JMUClassScheduleToiCalender
-将集美大学课程表转换成iCalendar文件
+# JMU Class Schedule To iCalender
+将集美大学课程表转换成 [iCalendar](https://tools.ietf.org/html/rfc2445) 文件，方便将课程表导入到 Android、iOS 等设备中，实时查看今日的课程。
+
+程序仅支持在 Python3 环境下运行。
+
+# Usage
+
+如果你的 Python 默认不是 Python3，请使用 `pip3` 与 `python3` 替换下文命令中的 `pip` 与 `python`。
+
+```bash
+git clone https://github.com/LGiki/JMUClassScheduleToiCalender.git
+cd JMUClassScheduleToiCalender
+pip install -r requirements.txt
+python jmu_class_schedule_to_ics.py
+```
+
+# Screenshot
+
+将生成的 ics 文件导入到 iOS 中可以得到以下效果：
+
+![iOS Screenshot](screenshot/ios.png)
+
+添加“日程”小组件到负一屏可以很方便地知道今日课程。
+
---
+++ jmu_class_schedule_to_ics.py
@@ -0,0 +1,114 @@
+import requests
+from icalendar import Calendar, Event
+import pytz
+import datetime
+import hashlib
+import os
+import re
+
+
+def sha1(data):
+    return hashlib.sha1(data.encode('utf-8')).hexdigest()
+
+
+def get_jmu_sid(username, password):
+    password = sha1(password)
+    request_data = {'flag': 1, 'unitid': 55, 'encrypt': 1, 'imgcode': '',
+                    'account': username, 'appid': '273', 'password': password}
+    request_result = requests.post(
+        'http://oa99.jmu.edu.cn/v2/passport/api/user/login1', request_data)
+    if request_result.status_code != 200:
+        return -1
+    else:
+        return request_result.json()['sid']
+
+
+def get_schedule_to_json(sid):
+    request_result = requests.get(
+        'http://210.34.130.89/CourseSchedule/StudentCourseSchedule?sid=' + sid)
+    if request_result.status_code != 200:
+        return -1
+    else:
+        return request_result.json()
+
+
+def get_course_take_weeks(all_week):  # 返回课程上课的周
+    course_take_weeks = []
+    is_interval = 0
+    if '单周' in all_week:
+        is_interval = 1
+    elif '双周' in all_week:
+        is_interval = 2
+    match_object = re.match(r'(\d+)-(\d+)', all_week)
+    if match_object:
+        start_week = int(match_object.group(1))
+        end_week = int(match_object.group(2))
+        for i in range(start_week, end_week + 1):
+            if is_interval == 0 or (is_interval == 1 and i % 2 != 0) or (is_interval == 2 and i % 2 == 0):
+                course_take_weeks.append(i)
+    return course_take_weeks
+
+
+def get_course_date(first_day_date_str, course_in_day_of_week, which_week):  # 返回上课的日期
+    first_day_date = datetime.datetime.strptime(
+        first_day_date_str, r'%Y-%m-%d')
+    return first_day_date + datetime.timedelta(days=(which_week - 1) * 7 + (course_in_day_of_week - 1))
+
+
+def get_course_take_time(course_time):  # 返回上课时间
+    course_time_table = {'12': [[8, 0, 0], [9, 35, 0]],
+                         '34': [[10, 5, 0], [11, 40, 0]],
+                         '56': [[14, 0, 0], [15, 35, 0]],
+                         '78': [[15, 55, 0], [17, 30, 0]],
+                         '910': [[19, 0, 0], [20, 35, 0]]}
+    return course_time_table[course_time][0], course_time_table[course_time][1]
+
+
+def main():
+    time_zone = pytz.timezone('Asia/Shanghai')
+    username = input('Please input your JiDaTong username:')
+    password = input('Please input your JiDaTong password:')
+    first_day_date_str = input('Please input first day of term(Format: Year-month-day):')
+    sid = get_jmu_sid(username, password)
+    if sid == -1:
+        print('Your username or password is error, please try again.')
+        exit(1)
+    print('Your JiDaTong sid is:', sid)
+    schedule_json = get_schedule_to_json(sid)
+    if schedule_json == -1:
+        print('Program met some wrong, please wait and try again.')
+        exit(1)
+    name = schedule_json['Name']
+    class_name = schedule_json['className']
+    courses = schedule_json['courses']
+    calendar = Calendar()
+    calendar.add('prodid', '-//My calendar product//mxm.dk//')
+    calendar.add('version', '2.0')
+    for course in courses:
+        course_in_day_of_week = int(course['couDayTime'])  # 课程在星期几
+        course_take_weeks = get_course_take_weeks(course['allWeek'])
+        course_begin_time, course_end_time = get_course_take_time(course['coudeTime'])
+        for week in course_take_weeks:
+            course_date = get_course_date(first_day_date_str, course_in_day_of_week, week)
+            event = Event()
+            event.add('summary', course['couName'])
+            event.add('dtstart',
+                      datetime.datetime(course_date.year, course_date.month, course_date.day, course_begin_time[0],
+                                        course_begin_time[1], course_begin_time[2], tzinfo=time_zone))
+            event.add('dtend',
+                      datetime.datetime(course_date.year, course_date.month, course_date.day, course_end_time[0],
+                                        course_end_time[1], course_end_time[2], tzinfo=time_zone))
+            event.add('dtstamp',
+                      datetime.datetime(course_date.year, course_date.month, course_date.day, course_begin_time[0],
+                                        course_begin_time[1], course_begin_time[2], tzinfo=time_zone))
+            event.add('location', course['couRoom'])
+            calendar.add_component(event)
+    output_file_name = '{} - {}.ics'.format(class_name, name)
+    output_file = open(output_file_name, 'wb')
+    output_file.write(calendar.to_ical())
+    output_file.close()
+    print('Success write your calendar to', output_file_name)
+
+
+if __name__ == '__main__':
+    main()
